6b21c3053c4d0113b55043cf9cb9ad7bcfc72909
ByungHoon YOO <62757768+bhyoo99@users.noreply.github.com>
2022-02-03 23:06:22 +0900

    [Add] ScrollView hook, use example

---
+++ use-draggable-scroll.ts
@@ -0,0 +1,48 @@
+import { RefObject, useEffect, useRef, useMemo } from "react";
+import { Platform, findNodeHandle } from "react-native";
+import type { ScrollView } from "react-native";
+import mergeRefs from "react-merge-refs";
+
+type Props<Scrollable extends ScrollView = ScrollView> = {
+  cursor?: string;
+  outerRef?: RefObject<Scrollable>;
+};
+
+export function useDraggableScroll<Scrollable extends ScrollView = ScrollView>({
+  outerRef,
+  cursor = "grab",
+}: Props<Scrollable> = {}) {
+  const ref = useRef<Scrollable>(null);
+
+  useEffect(
+    function listeners() {
+      if (Platform.OS !== "web" || !ref.current) {
+        return;
+      }
+      const slider = findNodeHandle(ref.current) as unknown as HTMLDivElement;
+      if (!slider) {
+        return;
+      }
+
+      const wheel = (e: WheelEvent) => {
+        e.preventDefault();
+        slider.scrollLeft += e.deltaY;
+      };
+      slider.addEventListener("wheel", wheel);
+
+      return () => {
+        slider.removeEventListener("wheel", wheel);
+      };
+    },
+    [cursor]
+  );
+
+  const refs = useMemo(
+    () => mergeRefs(outerRef ? [ref, outerRef] : [ref]),
+    [ref, outerRef]
+  );
+
+  return {
+    refs,
+  };
+}
